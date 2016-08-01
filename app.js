f6a924d125c9a48c440794df0208597ed7bcbf9c
darkenvy <darkenvy6@gmail.com>
2016-07-31 23:09:44 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var recentInput;
+  document.addEventListener('click', function(e) {
+    if (e.target.tagName == 'INPUT') {
+      recentInput = e;
+      console.log(e.target);
+    }
+  })
+
+  document.getElementsByClassName('p')[0].addEventListener('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    // console.log(e.target.tagName);
+    if (e.target.tagName == 'LI') {
+      recentInput.target.value = recentInput.target.value + e.target.innerHTML;
+    }
+    // console.log(recentInput.focus());
+  })
+})
+
+
+
+// --------- Bookmarklet Working example ----------
+// var mydiv = document.createElement('div');
+// mydiv.innerHTML = '<div style="background-color: yellow; position: relative;">' + 
+//   '<div style="background-color: blue; position: fixed; top: 0; left: 0; color: white;' + 
+//   ' width: 100%;">Lorem Ipsum</div></div>';
+// document.body.appendChild(mydiv);
+
+// document.activeElement();
+
+
+
+// --------- Add CSS Rule example ----------
+// javascript: (function() {
+//   var newcss = window.prompt("new css rule:");
+//   if ("\v" == "v") {
+//     document.createStyleSheet().cssText = newcss
+//   } else {
+//     var tag = document.createElement("style");
+//     tag.type = "text/css";
+//     document.getElementsByTagName("head")[0].appendChild(tag);
+//     tag[(typeof document.body.style.WebkitAppearance == "string") ? "innerText" : "innerHTML"] = newcss
+//   }
+// })();
